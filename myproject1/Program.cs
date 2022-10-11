884a775ceaa1102e6c499936e4ac2a172f7f268f
karanmaurya15 <karanmaurya9663@gmail.com>
2022-10-11 13:40:45 +0530

    added test2 file

--- myproject1/Program.cs
+++ myproject1/Program.cs
@@ -5,6 +5,8 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, INDIA!");
+            Console.WriteLine("Hello, KK!");
             Console.Read();
         }
     }

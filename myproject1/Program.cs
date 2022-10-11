16a38deb8ba45fa2667f25307dba24b0a817e282
karanmaurya15 <karanmaurya9663@gmail.com>
2022-10-11 09:00:01 +0530

    added files

---
+++ myproject1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace myproject1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.Read();
+        }
+    }
+}
